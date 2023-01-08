cc9e6ee03fc7862379235f3fef1504b2ea7daaa4
Amit Maraj <amit.maraj@gmail.com>
2023-01-07 21:30:03 -0500

    first commit

---
+++ src/components/FallingLeaves.jsx
@@ -0,0 +1,88 @@
+import resolvePath from "@src/utils/resolvePath";
+
+export default () => {
+  return (
+    <>
+      <section className='leaves'>
+        <div class='set'>
+          <div>
+            <img src='assets/images/leaves/leaves1.png' />
+          </div>
+          <div>
+            <img src='assets/images/leaves/leaves2.png' />
+          </div>
+          <div>
+            <img src='assets/images/leaves/leaves3.png' />
+          </div>
+          <div>
+            <img src='assets/images/leaves/leaves4.png' />
+          </div>
+          <div>
+            <img src='assets/images/leaves/leaves1.png' />
+          </div>
+          <div>
+            <img src='assets/images/leaves/leaves2.png' />
+          </div>
+          <div>
+            <img src='assets/images/leaves/leaves3.png' />
+          </div>
+          <div>
+            <img src='assets/images/leaves/leaves4.png' />
+          </div>
+        </div>
+        <div class='set set2'>
+          <div>
+            <img src='assets/images/leaves/leaves1.png' />
+          </div>
+          <div>
+            <img src='assets/images/leaves/leaves2.png' />
+          </div>
+          <div>
+            <img src='assets/images/leaves/leaves3.png' />
+          </div>
+          <div>
+            <img src='assets/images/leaves/leaves4.png' />
+          </div>
+          <div>
+            <img src='assets/images/leaves/leaves1.png' />
+          </div>
+          <div>
+            <img src='assets/images/leaves/leaves2.png' />
+          </div>
+          <div>
+            <img src='assets/images/leaves/leaves3.png' />
+          </div>
+          <div>
+            <img src='assets/images/leaves/leaves4.png' />
+          </div>
+        </div>
+        <div class='set set3'>
+          <div>
+            <img src='assets/images/leaves/leaves1.png' />
+          </div>
+          <div>
+            <img src='assets/images/leaves/leaves2.png' />
+          </div>
+          <div>
+            <img src='assets/images/leaves/leaves3.png' />
+          </div>
+          <div>
+            <img src='assets/images/leaves/leaves4.png' />
+          </div>
+          <div>
+            <img src='assets/images/leaves/leaves1.png' />
+          </div>
+          <div>
+            <img src='assets/images/leaves/leaves2.png' />
+          </div>
+          <div>
+            <img src='assets/images/leaves/leaves3.png' />
+          </div>
+          <div>
+            <img src='assets/images/leaves/leaves4.png' />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
